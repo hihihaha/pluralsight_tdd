dd0a03292be329c89b0bb068d17538d1effbfbbc
hihihaha <ivan.vekshin@gmail.com>
2016-09-25 08:45:39 +0300

    Complited screencast #4 from controllers tests

--- (03) Acceptance Tests/i-rock/spec/controllers/achievements_controller_spec.rb
+++ (03) Acceptance Tests/i-rock/spec/controllers/achievements_controller_spec.rb
@@ -23,4 +23,34 @@ describe AchievementsController do
       expect(assigns(:achievement)).to eq(achievement)
     end
   end
+
+  describe "POST create" do
+    context "valid data" do
+      let(:valid_data) { FactoryGirl.attributes_for(:public_achievement) }
+      
+      it "redirects to achievements#show" do
+        post :create, params: { achievement: valid_data }
+        expect(response).to redirect_to(achievement_path(assigns[:achievement]))
+      end
+      it "create new achievement in database" do
+        expect {
+          post :create, params: { achievement: valid_data }
+        }.to change(Achievement, :count).by(1)
+      end
+    end
+    
+    context "invalid data" do
+      let(:invalid_data) { FactoryGirl.attributes_for(:public_achievement, title: "") }
+
+      it "renders :new template" do
+        post :create, params: { achievement: invalid_data }
+        expect(response).to render_template(:new)
+      end
+      it "does not create new achievement in database" do
+        expect {
+          post :create, params: { achievement: invalid_data }         
+        }.not_to change(Achievement, :count)
+      end
+    end
+  end
 end
