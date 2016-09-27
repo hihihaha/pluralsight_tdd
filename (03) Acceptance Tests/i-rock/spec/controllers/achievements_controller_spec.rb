768b16760ca8bd37c15377b711008d86defc8acc
hihihaha <ivan.vekshin@gmail.com>
2016-09-27 09:43:00 +0300

    Complited screencast #6 from controllers tests

--- (03) Acceptance Tests/i-rock/spec/controllers/achievements_controller_spec.rb
+++ (03) Acceptance Tests/i-rock/spec/controllers/achievements_controller_spec.rb
@@ -29,6 +29,40 @@ describe AchievementsController do
     end
   end
 
+  describe "PUT update" do
+    let(:achievement) { FactoryGirl.create(:public_achievement) }
+
+    context "valid data" do
+      let(:valid_data) { FactoryGirl.attributes_for(:public_achievement, title: "Updated Title") }
+
+      it "redirects to achievements#show" do
+        put :update, params: { id: achievement, achievement: valid_data }
+        expect(response).to redirect_to(achievement_path())
+      end
+
+      it "updates achievement in database" do
+        put :update, params: { id: achievement, achievement: valid_data }
+        achievement.reload
+        expect(achievement.title).to eq("Updated Title")
+      end
+    end
+
+    context "invalid data" do
+      let(:invalid_data) { FactoryGirl.attributes_for(:public_achievement, title: "", description: "new") }
+
+      it "renders :edit template" do
+        put :update, params: { id: achievement, achievement: invalid_data }
+        expect(response).to render_template(:edit)
+      end
+
+      it "doesn't update achievement in database" do
+        put :update, params: { id: achievement, achievement: invalid_data }
+        achievement.reload
+        expect(achievement.description).not_to eq("new")
+      end
+    end
+  end
+
   describe "GET new" do
     it "renders :new template" do
       get :new
@@ -84,6 +118,21 @@ describe AchievementsController do
           post :create, params: { achievement: invalid_data }         
         }.not_to change(Achievement, :count)
       end
+    end
+  end
+
+  describe "DELETE destroy" do
+    let!(:achievement) { FactoryGirl.create(:public_achievement, title: "will be deleted") }
+
+    it "redirects to achievements#index" do
+      delete :destroy, params: { id: achievement }
+      expect(response).to redirect_to(achievements_path)
+    end
+
+    it "deletes achievement from database" do
+      expect {
+        delete :destroy, params: { id: achievement }
+      }.to change(Achievement, :count).by(-1)
     end
   end
 end
