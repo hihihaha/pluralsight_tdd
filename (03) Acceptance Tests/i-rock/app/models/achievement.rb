f237f0b7b3e66a3d63eb5bd79040f1f796e01fb4
hihihaha <ivan.vekshin@gmail.com>
2016-09-27 12:20:07 +0300

    Complited screencast #9 from controllers tests

--- (03) Acceptance Tests/i-rock/app/models/achievement.rb
+++ (03) Acceptance Tests/i-rock/app/models/achievement.rb
@@ -1,5 +1,8 @@
 class Achievement < ApplicationRecord
+  belongs_to :user, optional: true
+
   validates :title, presence: true
+
   enum privacy: [ :public_access, :private_access, :friends_access ]
 
   def description_html
