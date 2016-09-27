a974201e839c707be54b192decd067ad3f450d40
hihihaha <ivan.vekshin@gmail.com>
2016-09-27 07:43:36 +0300

    Complited screencast #5 from controllers tests

--- (03) Acceptance Tests/i-rock/spec/controllers/achievements_controller_spec.rb
+++ (03) Acceptance Tests/i-rock/spec/controllers/achievements_controller_spec.rb
@@ -1,11 +1,40 @@
 require "rails_helper"
 
 describe AchievementsController do
+  describe "GET index" do
+    it "renders :new template" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it "assigns only public achievements to template" do
+      get :index
+      public_achievement = FactoryGirl.create(:public_achievement)
+      private_achievement = FactoryGirl.create(:private_achievement)
+      expect(assigns(:achievements)).to match_array([public_achievement])
+    end
+  end
+
+  describe "GET edit" do
+    let(:achievement) { FactoryGirl.create(:public_achievement) }
+
+    it "renders :edit template" do
+      get :show, params: { id: achievement }
+      expect(response).to render_template(:show)
+    end
+
+    it "assigns requested achievement to @achievement" do
+      get :show, params: { id: achievement }
+      expect(assigns(:achievement)).to eq(achievement)
+    end
+  end
+
   describe "GET new" do
     it "renders :new template" do
       get :new
       expect(response).to render_template(:new)
     end
+
     it "assigns new Achievement to @achievement" do
       get :new
       expect(assigns(:achievement)).to be_a_new(Achievement) 
@@ -14,10 +43,12 @@ describe AchievementsController do
 
   describe "GET show" do
     let(:achievement) { FactoryGirl.create(:public_achievement) }
+
     it "renders :show template" do
       get :show, params: { id: achievement }
       expect(response).to render_template(:show)
     end
+
     it "assigns requested achievement to @achievement" do
       get :show, params: { id: achievement }
       expect(assigns(:achievement)).to eq(achievement)
@@ -32,6 +63,7 @@ describe AchievementsController do
         post :create, params: { achievement: valid_data }
         expect(response).to redirect_to(achievement_path(assigns[:achievement]))
       end
+
       it "create new achievement in database" do
         expect {
           post :create, params: { achievement: valid_data }
@@ -46,6 +78,7 @@ describe AchievementsController do
         post :create, params: { achievement: invalid_data }
         expect(response).to render_template(:new)
       end
+
       it "does not create new achievement in database" do
         expect {
           post :create, params: { achievement: invalid_data }         
