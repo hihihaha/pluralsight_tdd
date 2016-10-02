8c7bcd9bc4c6eac34937ba9c47f9c0edd54f5699
hihihaha <ivan.vekshin@gmail.com>
2016-10-02 17:27:15 +0300

    Complited screencast #3 from models tests

--- app_for_testing/i-rock/spec/models/achievement_spec.rb
+++ app_for_testing/i-rock/spec/models/achievement_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "validations" do
+    it "requires title" do
+      achievement = Achievement.new(title: "")
+      expect(achievement.valid?).to be_falsy
+    end
+
+    it "requires title to be unique for one user" do
+      user = FactoryGirl.create(:user)
+      achievemnent = FactoryGirl.create(:public_achievement, title: "First Achievement", user: user)
+      new_achievement = Achievement.new(title: "First Achievement", user: user)
+      expect(new_achievement.valid?).to be_falsy
+    end
+
+    it "allows differnet users to have achievemnents with identical titles" do
+      user_1 = FactoryGirl.create(:user)
+      user_2 = FactoryGirl.create(:user)
+      user_1_achievement = FactoryGirl.create(:public_achievement, title: "Achievement", user: user_1)
+      user_2_achievement = Achievement.new(title: "Achievement", user: user_2)
+      expect(user_2_achievement.valid?).to be_truthy
+    end
+  end
 end
