f007f8c1baf7963caddd2127141da9771ae033de
hihihaha <ivan.vekshin@gmail.com>
2016-10-02 18:17:30 +0300

    Complited screencast #4 from models tests

--- app_for_testing/i-rock/spec/rails_helper.rb
+++ app_for_testing/i-rock/spec/rails_helper.rb
@@ -5,7 +5,9 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'shoulda/matchers'
 require 'devise'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -57,4 +59,11 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.include Devise::Test::ControllerHelpers, type: :controller
+end
+
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :rspec
+    with.library :rails
+  end
 end
