5d0a06bc7c0c75747aaec8469d8cdd527f2619a2
hihihaha <ivan.vekshin@gmail.com>
2016-10-02 18:17:30 +0300

    Complited screencast #4 from models tests

--- app_for_testing/i-rock/spec/models/achievement_spec.rb
+++ app_for_testing/i-rock/spec/models/achievement_spec.rb
@@ -2,24 +2,9 @@ require 'rails_helper'
 
 RSpec.describe Achievement, type: :model do
   describe "validations" do
-    it "requires title" do
-      achievement = Achievement.new(title: "")
-      expect(achievement.valid?).to be_falsy
-    end
-
-    it "requires title to be unique for one user" do
-      user = FactoryGirl.create(:user)
-      achievemnent = FactoryGirl.create(:public_achievement, title: "First Achievement", user: user)
-      new_achievement = Achievement.new(title: "First Achievement", user: user)
-      expect(new_achievement.valid?).to be_falsy
-    end
-
-    it "allows differnet users to have achievemnents with identical titles" do
-      user_1 = FactoryGirl.create(:user)
-      user_2 = FactoryGirl.create(:user)
-      user_1_achievement = FactoryGirl.create(:public_achievement, title: "Achievement", user: user_1)
-      user_2_achievement = Achievement.new(title: "Achievement", user: user_2)
-      expect(user_2_achievement.valid?).to be_truthy
-    end
+    it { should validate_presence_of(:title) }
+    it { should validate_uniqueness_of(:title).scoped_to(:user_id).with_message("you can't have two achievements with the same title") }
+    it { should validate_presence_of(:user) }
+    it { should belong_to(:user) }
   end
 end
